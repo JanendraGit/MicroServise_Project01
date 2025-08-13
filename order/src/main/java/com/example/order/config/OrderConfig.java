c4d6a073f71f5381622775aab3f16bdf05de7cd6
janendra de silva <janendra.silva2001@gmail.com>
2025-08-13 10:19:59 +0530

    add somthing

--- order/src/main/java/com/example/order/config/OrderConfig.java
+++ order/src/main/java/com/example/order/config/OrderConfig.java
@@ -3,6 +3,7 @@ package com.example.order.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -16,5 +17,10 @@ public class OrderConfig {
     @Bean
     public WebClient InventoryWebClient() {
         return webClientBuilder().baseUrl("http://inventory").build();
+    }
+
+    @Bean
+    public ModelMap modelMap(){
+        return new ModelMap();
     }
 }
